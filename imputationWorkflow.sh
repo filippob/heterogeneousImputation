b6c90853f78f44fea4c2c240d7c6342b8f6c312a
filippob <filippo.biscarini@gmail.com>
2020-05-08 11:08:49 +0200

    uncommentd rm command at the end of imputationWorkflow.sh

--- imputationWorkflow.sh
+++ imputationWorkflow.sh
@@ -152,5 +152,5 @@ echo "#######################################"
 ## STEP 4
 ## parsing results
 $RPATH --vanilla ${MAINPATH}/heterogeneousImputation/scripts/parseResults.R originalRaw.raw imputed.raw indexes.txt $( basename $INPUTFILE) $MAINPATH
-#rm artificialMissing.ped artificialMissing.map originalRaw.raw imputed.raw *.vcf imputed.ped imputed.map *.gz subset.ped subset.log subset.map subset.frq freq.frq *.nosex originalRaw.log freq.log transposed.*
+rm artificialMissing.ped artificialMissing.map originalRaw.raw imputed.raw *.vcf imputed.ped imputed.map *.gz subset.ped subset.log subset.map subset.frq freq.frq *.nosex originalRaw.log freq.log transposed.*
 
